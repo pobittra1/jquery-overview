bbc20dd5622b476525dc1ecdc01b4d8dfeedc4f9
pobittra <pobittrachandraroy2001@gmail.com>
2025-03-20 14:57:58 +0600

    usage of .val(), .attr(), .css() methods

--- jq.js
+++ jq.js
@@ -2,16 +2,25 @@
 $(document).ready(function () {
     $("h1").addClass("heading-style");
 
-
-
-    // get a heading content using .html() method & #headingOne id
+    // Get a heading content using .html() method & #headingOne id
     let headingContent = $("#headingOne").html();
-    console.log(headingContent);
+    // console.log(headingContent);
 
-    //also can use .text() method to get the content
+    // Also can use .text() method to get the content
     let headingContent2 = $("#headingOne").text();
-    console.log(headingContent2);
-});
+    // console.log(headingContent2);
 
+    // .val(), .attr(), .css() methods
+    let inputVal = $("#inputFieldOne").val();
+    // console.log(inputVal);
+
+    // We can set also attr type value using this code. First parameter is attribute name and second is value
+    let inputAttr = $("#inputFieldOne").attr("type", "email");
+    // console.log(inputAttr);
+
+    // get css property value of headingOne
+    let cssValue = $("#headingOne").css("color");
+    // console.log(cssValue);
+});
 
 
