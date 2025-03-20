698f0765e1d8eb98d20ebe05b0ae527d6ddc5eff
pobittra <pobittrachandraroy2001@gmail.com>
2025-03-20 14:44:55 +0600

    // get a heading content using .html() method & #headingOne id

--- jq.js
+++ jq.js
@@ -1,4 +1,13 @@
 // Add a class to all <h1> elements using jQuery
 $(document).ready(function () {
     $("h1").addClass("heading-style");
+
+
+
+    // get a heading content using .html() method & #headingOne id
+    let headingContent = $("#headingOne").html();
+    console.log(headingContent);
 });
+
+
+
